cdd82700821192eca488e897bf0cd6257e1c689e
Vaishali <vaishalithakur614@gmail.com>
2020-04-18 02:02:25 +0530

    Create 3. numberOfIslands.cpp

---
+++ Week 3: April 15th - April 21st/3. numberOfIslands.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void callBFS(vector<vector<char>>& grid, int i, int j){
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        callBFS(grid, i+1, j);  // up
+        callBFS(grid, i-1, j);  // down
+        callBFS(grid, i, j-1);  // left
+        callBFS(grid, i, j+1);  // right
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    callBFS(grid, i, j);
+                }
+            }
+        }
+        
+        return count;
+    }
+};
